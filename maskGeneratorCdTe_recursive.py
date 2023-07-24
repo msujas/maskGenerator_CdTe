9aa425892752a3e4d4caf0e9fbba9c90bc2e474a
Kenneth Marshall <kenny.mrshll@gmail.com>
2023-07-24 11:45:59 +0200

    destination folder for recursive script

--- maskGeneratorCdTe_recursive.py
+++ maskGeneratorCdTe_recursive.py
@@ -4,7 +4,6 @@ import fabio
 import numpy as np
 from glob import glob
 import os, re
-import matplotlib.pyplot as plt
 from integrationFunctions import clearPyFAI_header, gainCorrection, bubbleHeader
 
 
@@ -14,6 +13,8 @@ mask = r'C:\Users\kenneth1a\Documents\beamlineData\May2023\Si00_tilt\pos05_mask.
 gainFile = r'C:\Users\kenneth1a\Documents\beamlineData\May2023\compare/afterRestart_solidAngle.edf'
 #gainFile = r'C:\Users\kenneth1a\Documents\beamlineData\Feb2023_gainMap/calculatedGainMap_48p6keV_2023-02-21_2.edf'
 #gainFile = r'C:\Users\kenneth1a\Documents\beamlineData\Feb2023_gainMap/calculatedGainMap_48p6keV_filtered_kpm_2023-04-13.edf'
+dest = direc
+
 os.chdir(direc)
 mask = fabio.open(mask).data
 poni = pyFAI.load(poni)
@@ -42,16 +43,17 @@ for root, dirs, files in os.walk(direc):
 
     subdir = f'xye_{stdevs}stdev/'
     subdirGain = f'xye_{stdevs}stdev_gainCorr/'
-    if not os.path.exists(f'{root}/{subdir}/'):
-        os.makedirs(f'{root}/{subdir}')
-    if not os.path.exists(f'{root}/{avdir}/xye/'):
-        os.makedirs(f'{root}/{avdir}/xye/')
-    if not os.path.exists(f'{root}/{subdir}/{avdir}/'):
-        os.makedirs(f'{root}/{subdir}/{avdir}/')
-    if not os.path.exists(f'{root}/{subdirGain}/'):
-        os.makedirs(f'{root}/{subdirGain}')
-    if not os.path.exists(f'{root}/{subdirGain}/{avdir}/'):
-        os.makedirs(f'{root}/{subdirGain}/{avdir}/')
+    outfolder = root.replace(direc,dest)
+    if not os.path.exists(f'{outfolder}/{subdir}/'):
+        os.makedirs(f'{outfolder}/{subdir}')
+    if not os.path.exists(f'{outfolder}/{avdir}/xye/'):
+        os.makedirs(f'{outfolder}/{avdir}/xye/')
+    if not os.path.exists(f'{outfolder}/{subdir}/{avdir}/'):
+        os.makedirs(f'{outfolder}/{subdir}/{avdir}/')
+    if not os.path.exists(f'{outfolder}/{subdirGain}/'):
+        os.makedirs(f'{outfolder}/{subdirGain}')
+    if not os.path.exists(f'{outfolder}/{subdirGain}/{avdir}/'):
+        os.makedirs(f'{outfolder}/{subdirGain}/{avdir}/')
     i1 = fabio.open(cbfs[0]).data
     dataset = np.empty(shape = (*i1.shape,len(cbfs)))
     for c,file in enumerate(cbfs):
@@ -79,14 +81,14 @@ for root, dirs, files in os.walk(direc):
         masks[c] = np.where(array > median+stdevs*stdev,1,mask)
 
         xyefile = file.replace('.cbf','.xye')
-        outputfile = f'{root}/{subdir}/{xyefile}'
+        outputfile = f'{outfolder}/{subdir}/{xyefile}'
         x,y,e = poni.integrate1d(data = dataset[:,:,c], filename = outputfile,mask = masks[c],polarization_factor = 0.99,unit = '2th_deg',
                     correctSolidAngle = True, method = 'bbox',npt = 5000, error_model = 'poisson',safe = False)
         print(outputfile)
         if doGain:
             arrayGC = dataset[:,:,c]/gainArray
             arrayGC = np.where(gainArray < 0, -1, arrayGC)
-            outputfileGC = f'{root}/{subdirGain}/{xyefile}'
+            outputfileGC = f'{outfolder}/{subdirGain}/{xyefile}'
             xg,yg,eg = poni.integrate1d(data = arrayGC, filename = outputfileGC,mask = masks[c],polarization_factor = 0.99,unit = '2th_deg',
             correctSolidAngle = True, method = 'bbox',npt = 5000, error_model = 'poisson',safe = False)
         if c == 0:
@@ -102,12 +104,12 @@ for root, dirs, files in os.walk(direc):
     avim = np.nanmean(dataset2, axis = 2)
     avim = np.where(np.isnan(avim), -2, avim)
     im = fabio.cbfimage.CbfImage(avim)
-    im.save(f'{root}/{avdir}/average.cbf')
+    im.save(f'{outfolder}/{avdir}/average.cbf')
 
     mask_av = np.where(avim < 0, 1, 0)
     basefilename = os.path.basename(cbfs[-1])
     shortbasename = re.sub('_[0-9][0-9][0-9][0-9]p','',basefilename).replace('.cbf','')
-    outfile = f'{root}/{avdir}/xye/{shortbasename}_average.xye'
+    outfile = f'{outfolder}/{avdir}/xye/{shortbasename}_average.xye'
     outfile_2d = outfile.replace('.xye','_pyfai.edf')
     poni.integrate1d(data = avim, filename = outfile,mask = mask_av,polarization_factor = 0.99,unit = '2th_deg',
                 correctSolidAngle = True, method = 'bbox',npt = 5000, error_model = 'poisson', safe = False)
@@ -118,8 +120,8 @@ for root, dirs, files in os.walk(direc):
     if doGain:
         avimGain = gainCorrection(avim,gainArray)
         imGain = fabio.cbfimage.CbfImage(avimGain)
-        imGain.save(f'{root}/{avdir}/average_gainCorrected.cbf')
-        outfileGC = f'{root}/{avdir}/xye/{shortbasename}_average_gainCorrected.xye'
+        imGain.save(f'{outfolder}/{avdir}/average_gainCorrected.cbf')
+        outfileGC = f'{outfolder}/{avdir}/xye/{shortbasename}_average_gainCorrected.xye'
         outfile_2dGC = outfileGC.replace('.xye','.edf')
         poni.integrate1d(data = avimGain, filename = outfileGC,mask = mask_av,polarization_factor = 0.99,unit = '2th_deg',
             correctSolidAngle = True, method = 'bbox',npt = 5000, error_model = 'poisson', safe = False)
