b0baae428183dde804c65b900270156b783a1c10
Kenneth Marshall <kenny.mrshll@gmail.com>
2024-03-29 11:26:50 +0100

    copyMonitor for pylatus to visitor

---
+++ copyMonitor.py
@@ -0,0 +1,33 @@
+from cryio.cbfimage import CbfHeader
+import os
+from glob import glob
+#import shutil
+
+sourcedir = r'X:\users\a311217'
+destdir = r'Z:\visitor\a311217\bm31\20240129\pylatus'
+
+def run(sourcedir,destdir):
+    for root,dirs, files in os.walk(sourcedir):
+        cbffiles = glob(f'{root}/*.cbf')
+        if len(cbffiles) == 0:
+            continue
+        
+        destfiles = [file.replace(sourcedir,destdir) for file in cbffiles]
+        for file,destfile in zip(cbffiles,destfiles):
+            header = CbfHeader(file)
+            try:
+                flux = int(header['Flux'])
+            except:
+                continue
+            print(file)
+            print(destfile)
+            if not os.path.exists(destfile):
+                #shutil.copy(file, destfile)
+                continue
+            destheader = CbfHeader(destfile)
+            destheader.header['Flux'] = flux
+            destheader.save_cbf(destfile)
+               
+if __name__ == '__main__':
+    run(sourcedir,destdir) 
+    
