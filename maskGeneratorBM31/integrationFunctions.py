bug fix averagefiles

maskGeneratorBM31/integrationFunctions.py
@@ -95,10 +95,12 @@ def makeMasks(dataset, files, baseMask, nstdevs = 3, plot = False):
             plt.show()
     return maskdct
 
-def averagefiles(basemask,pattern = '*.cbf', flatfield=None):
-    files = glob(pattern)
-    dirname = os.path.dirname(os.path.abspath(files[0]))
-    dataset, usedfiles = makeDataSet(files,f'{dirname}/badframes.log')
+def averagefiles(*files, basemask, flatfield=None):
+    filelsit = []
+    for f in files:
+        filelsit += glob(f)
+    dirname = os.path.dirname(os.path.abspath(filelsit[0]))
+    dataset, usedfiles = makeDataSet(filelsit,f'{dirname}/badframes.log')
     masks = makeMasks(dataset, usedfiles, basemask)
     for i in range(dataset.shape[2]):
         dataset[:,:,i] = np.where(masks[i] >0, np.nan, dataset[:,:,i])
@@ -109,7 +111,7 @@ def averagefiles(basemask,pattern = '*.cbf', flatfield=None):
     im = CbfImage()
     im.array = av
     im.header['Flux'] = 1
-    basefilename = os.path.basename(files[-1])
+    basefilename = os.path.basename(filelsit[-1])
     shortbasename = re.sub('_[0-9][0-9][0-9][0-9]p','',basefilename).replace('.cbf','')
     im.save_cbf(f'{avdir}/{shortbasename}.cbf')
     if flatfield:
@@ -124,13 +126,13 @@ def averagefiles(basemask,pattern = '*.cbf', flatfield=None):
 def averagefilescli():
     ap = argparse.ArgumentParser()
     ap.add_argument('-m','--mask', type=str, default=None, help='maskfile used for averageing')
-    ap.add_argument('-fp','--filepattern', type = str, help = 'pattern used to search file (default "*.cbf")', default='*.cbf')
+    ap.add_argument('files', nargs='+', help = 'files to average')
     ap.add_argument('-ff','--flatfield', type=str, default=None, help='flat field (gain map) file used to correct image')
     args = ap.parse_args()
     mask = args.mask
-    fp = args.filepattern
+    files = args.files
     fffile = args.flatfield
-    averagefiles(mask, fp, fffile)
+    averagefiles(*files,basemask= mask,flatfield= fffile)
 
 
 